b077d1c807de39a8a1d971a3d42222c965b4ea34
vxunderground <57078196+vxunderground@users.noreply.github.com>
2022-08-22 06:06:57 -0500

    del

--- MSIL/Trojan/Win32/R/Trojan.Win32.Reconyc.efsu-49381cd9042bc3cc4ed4ef172003f6330ee39256d57a554e08499cf3b3054e87/AssemblyInfo.cs
+++
@@ -1,5 +0,0 @@
-﻿using System.Reflection;
-using System.Security.Permissions;
-
-[assembly: AssemblyVersion("0.0.0.0")]
-[assembly: SecurityPermission(SecurityAction.RequestMinimum, SkipVerification = true)]
